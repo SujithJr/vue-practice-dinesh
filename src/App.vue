8bd5906251bf1789233e1b969088b8b05b7f6b12
SujithJr <sujithsrt10@gmail.com>
2021-06-23 11:59:24 +0530

    added vuetify, set up first component skeleton

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+    <v-app>
+        <v-main>
+            <v-sheet class="c-navbar" width="300" style="position: fixed; left: 0px; height: 100vh; overflow-y: auto; border-right: 2px solid rgba(0,0,0,0.05)">
+                <v-list>
+                    <v-list-item>
+                        <v-list-item-content class="grey--text text--darken-2 text-h6 font-weight-regular">
+                            Vue.js Practice
+                        </v-list-item-content>
+                    </v-list-item>
+                    <v-list-item v-for="(link, index)  in links" :key="index" :to="link.path">
+                        {{ link.label }}
+                    </v-list-item>
+                </v-list>
+            </v-sheet>
+            <v-sheet width="calc(100vw - 312px)" class="ml-auto px-8 mt-8">
+                <router-view />
+            </v-sheet>
+        </v-main>
+    </v-app>
+</template>
+
+<script>
+export default {
+    name: 'App',
+
+    data: () => ({
+        links: [
+            { label: 'Custom Events', path: '/' },
+        ]
+    })
+}
+</script>
+
+<style scoped>
+.c-navbar a {
+    border-bottom: 1px solid #fff !important;
+}
+.c-navbar .v-list-item--active {
+    color: #2e719e !important;
+    border-bottom: 1px solid #047083 !important;
+}
+</style>
---
+++ src/views/CustomEvents.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h2 class="text-h4">Custom Events</h2>
+        <v-divider class="grey lighten-3 my-6"></v-divider>
+        <div>
+            <!-- Use the component here -->
+        </div>
+    </div>
+</template>
+
+<script>
+// Import component here
+export default {
+    components: {
+        // Add component name here
+    }
+}
+</script>
